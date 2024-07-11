ae12b1e3e0218a1c2c8500ea30578ba708ece995
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-07-11 22:57:26 +1000

    admin layout glassmorphism effect

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,51 +1,52 @@
-import Stack from "react-bootstrap/Stack";
 import { Link } from "react-router-dom";
+import { Stack } from "react-bootstrap";
 
 export const Sidebar = () => {
   return (
-    <Stack gap={1} className="fw-bold fs-5 text-primary shadow-lg vh-110">
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/dashboard">
-          Dashboard
-        </Link>
-      </div>
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/categories">
-          Categories
-        </Link>
-      </div>
-
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/sub-categories">
-          Sub-Categories
-        </Link>
-      </div>
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/products">
-          Products
-        </Link>
-      </div>
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/users">
-          Users
-        </Link>
-      </div>
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/orders">
-          Orders
-        </Link>
-      </div>
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/reviews">
-          Reviews
-        </Link>
-      </div>
-      <hr />
-      <div className="p-2">
-        <Link className="nav-link" to="/admin/admins">
-          Admins
-        </Link>
-      </div>
-    </Stack>
+    <div className="glassmorphism-sidebar">
+      <Stack gap={1} className="fw-bold fs-5 text-primary shadow-lg">
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/dashboard">
+            Dashboard
+          </Link>
+        </div>
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/categories">
+            Categories
+          </Link>
+        </div>
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/sub-categories">
+            Sub-Categories
+          </Link>
+        </div>
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/products">
+            Products
+          </Link>
+        </div>
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/users">
+            Users
+          </Link>
+        </div>
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/orders">
+            Orders
+          </Link>
+        </div>
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/reviews">
+            Reviews
+          </Link>
+        </div>
+        <hr />
+        <div className="p-2">
+          <Link className="nav-link" to="/admin/admins">
+            Admins
+          </Link>
+        </div>
+      </Stack>
+    </div>
   );
 };
